fc62032c22c31433553304a58f3e73cef390dd79
MaxxLou <82404067+MaxxLou@users.noreply.github.com>
2024-05-03 23:47:57 +0300

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,7 +6,7 @@ export default function Navigation() {
     <nav className="flex justify-between items-center h-[50px] px-5 shadow-lg shadow-sky-900/20 bg-gray-200 text-gray-700 ">
       <h3 className="font-bold">Github Search</h3>
       <span>
-        <Link to="/" className="mr-6">Home</Link>
+        <Link to="/home" className="mr-6">Home</Link>
         <Link to="/favourites">Favourites</Link>
       </span>
     </nav>
